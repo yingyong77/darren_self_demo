ef3254a83a174157c2dd9738337f260c3d7948c1
darren <2818663415@qq.com>
2020-09-28 18:31:56 +0800

    处理异常

--- demo-elasticsearch/demo-elasticsearch/src/main/java/com/sky/demo/elasticsearch/utils/ThreadLocal.java
+++ demo-elasticsearch/demo-elasticsearch/src/main/java/com/sky/demo/elasticsearch/utils/ThreadLocal.java
@@ -1,12 +1,12 @@
 package com.sky.demo.elasticsearch.utils;
 
 import com.sky.demo.elasticsearch.common.dto.ComputeDTO;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,10 +29,15 @@ public class ThreadLocal {
      * @return {@link List<ComputeDTO>}
      */
     @NotNull
-    @SneakyThrows
     private List<ComputeDTO> convertToComputeDTOS(List<ComputeDTO> computeDTOS) {
 
-        return computeDTOS.stream().peek(i -> i.setDate(dateFormat.get().parse("2020-09-28"))).collect(Collectors.toList());
+        return computeDTOS.stream().peek(i -> {
+            try {
+                i.setDate(dateFormat.get().parse("2020-09-28"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }).collect(Collectors.toList());
     }
 
 }
