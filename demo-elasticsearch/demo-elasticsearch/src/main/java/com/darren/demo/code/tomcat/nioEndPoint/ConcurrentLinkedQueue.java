779a2c51d290659addb025feea7a97ddbc9b6f0f
darren <2818663415@qq.com>
2021-07-29 18:01:04 +0800

    tomcat的线程结构

---
+++ demo-elasticsearch/demo-elasticsearch/src/main/java/com/darren/demo/code/tomcat/nioEndPoint/ConcurrentLinkedQueue.java
@@ -0,0 +1,25 @@
+package com.darren.demo.code.tomcat.nioEndPoint;
+
+import org.apache.tomcat.util.net.Acceptor;
+import org.apache.tomcat.util.net.NioEndpoint;
+
+/**
+ * poller的数量取决于处理器的核数
+ * <p>
+ * ConcurrentLinkedQueue 无界线程安全队列
+ * <p>
+ * tomcat7.0中poller用的是ConcurrentLinkedQueue
+ * tomcat9.0中poller用的是 {@link org.apache.tomcat.util.collections.SynchronizedQueue}
+ *
+ * @author : darren
+ * @date : 2021/7/16
+ */
+public class ConcurrentLinkedQueue {
+
+    Acceptor acceptor;
+
+    NioEndpoint.Poller poller;
+
+    java.util.concurrent.ConcurrentLinkedQueue concurrentLinkedQueue;
+
+}
