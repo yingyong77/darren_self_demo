49fa1e62d71b323899975fd586f5ee840869bd87
darren <2818663415@qq.com>
2021-08-12 15:36:18 +0800

    关于函数式编程Function的用法supplier consumer predicate function 的用法举例

--- demo-elasticsearch/demo-elasticsearch/src/main/java/com/darren/demo/utils/DynamicProProxy/SimpleProxyTest.java
+++ demo-elasticsearch/demo-elasticsearch/src/main/java/com/darren/demo/utils/DynamicProProxy/SimpleProxyTest.java
@@ -1,6 +1,9 @@
 package com.darren.demo.utils.DynamicProProxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Objects;
 
 /**
  * 动态代理类
@@ -11,14 +14,46 @@ import java.lang.reflect.Proxy;
  */
 public class SimpleProxyTest {
 
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
 
         WorkInstance workInstance = new WorkInstance();
 
-        WorkInvocationHandler workInvocationHandler = new WorkInvocationHandler(workInstance);
+        WorkInvocationHandler<WorkInstance> workInvocationHandler = new WorkInvocationHandler<>(workInstance);
 
+        //Proxy类就是用来创建一个代理对象的类，它提供了很多方法，但是我们最常用的是newProxyInstance方法
         CommonFunction commonFunction = (CommonFunction) Proxy.newProxyInstance(CommonFunction.class.getClassLoader(), new Class<?>[]{CommonFunction.class}, workInvocationHandler);
 
         commonFunction.doWork();
     }
+
+
+    /**
+     * 内置对象
+     * 可将这一部 做进去
+     * WorkInstance workInstance = new WorkInstance();
+     */
+    class WorkInvocationHandlerInnerBuilt implements InvocationHandler {
+
+        private CommonFunction commonFunction;
+
+        private WorkInstance workInstance = new WorkInstance();
+
+        private CommonFunction getInstance() {
+
+            if (!Objects.isNull(workInstance)) {
+                commonFunction = (CommonFunction) Proxy.newProxyInstance(CommonFunction.class.getClassLoader(), new Class<?>[]{CommonFunction.class}, this);
+            }
+            return commonFunction;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            return null;
+        }
+    }
+
 }
