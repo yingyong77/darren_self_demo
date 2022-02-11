d2efc89ec32e40dfdf6525f50cf943f833c1104f
darren <2>
2022-02-11 09:13:44 +0800

    常量池demo1

---
+++ demo-elasticsearch/demo-elasticsearch/src/test/java/com/jvm/constantPool/Demo1.java
@@ -0,0 +1,18 @@
+package com.jvm.constantPool;
+
+/**
+ * 常量池说明
+ * <p
+ * 编译时先将类转化为二进制字节码
+ * 由三部分组成 1:类基本信息(field,method,类元信息 构造器constructor) 2:常量池 3：类方法
+ * p>
+ *
+ * @author : darren
+ * @date : 2022/2/10
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+    }
+}
