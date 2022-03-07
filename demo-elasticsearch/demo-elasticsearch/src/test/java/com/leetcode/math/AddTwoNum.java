e8382a8f843b3302909019221553b5aa04b14efa
darren <2>
2022-03-07 09:09:38 +0800

    [Netty] ByteBuffer用法 FileChannel用法 ~文件的传输&&常见socket通信黏包半包问题解决

--- demo-elasticsearch/demo-elasticsearch/src/test/java/com/leetcode/math/AddTwoNum.java
+++ demo-elasticsearch/demo-elasticsearch/src/test/java/com/leetcode/math/AddTwoNum.java
@@ -1,13 +1,18 @@
 package com.leetcode.math;
 
 /**
- * 两数相加
+ * 两数相加- 逆序数
  *
  * @author : darren
  * @date : 2022/3/2
  */
 public class AddTwoNum {
 
+    /**
+     * @param l1
+     * @param l2
+     * @return
+     */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
         ListNode head = null, tail = null;
@@ -38,10 +43,61 @@ public class AddTwoNum {
         return head;
     }
 
+    public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        // 课程 A -》 链表 -> 基础篇
+        ListNode dummy = new ListNode();   //虚拟表头
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+
+            int total = x + y + carry;
+            curr.next = new ListNode(total % 10);
+            // bug 修复：视频中忘了移动 curr 指针了
+            curr = curr.next;
+            carry = total / 10;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry != 0) curr.next = new ListNode(carry);
+        return dummy.next;
+    }
+
+    /**
+     * 递归解法
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers3(ListNode l1, ListNode l2) {
+        return add(l1, l2, 0);
+    }
+
+    static ListNode add(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
+        }
+        int val = carry;
+
+        if (l1 != null) {
+            val += l1.val;
+        }
+        if (l2 != null) {
+            val += l2.val;
+        }
+        ListNode node = new ListNode(val % 10);
+        node.next = add(l1, l2, val / 10);
+        return node;
+    }
+
+
     public static void main(String[] args) {
 
         ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
         ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-        addTwoNumbers(l1, l2);
+        addTwoNumbers2(l1, l2);
     }
 }
--- demo-elasticsearch/demo-elasticsearch/src/test/java/com/netty/demo/TestByteBufferAllocate.java
+++ demo-elasticsearch/demo-elasticsearch/src/test/java/com/netty/demo/TestByteBufferAllocate.java
@@ -11,7 +11,7 @@ public class TestByteBufferAllocate {
     public static void main(String[] args) {
         //堆内存  java堆内存 读写效率低 收到GC的影响（标记清理 标记复制 标记整理）
         System.out.println(ByteBuffer.allocate(10).getClass());
-        //直接内存 读写效率高（少一次拷贝 ）  不会收到gc的影响  分配效率低
+        //直接内存 读写效率高（少一次拷贝 ）  不会收到gc的影响  分配效率低（操作系统的函数控制）使用不当会造成内存泄露
         System.out.println(ByteBuffer.allocateDirect(10).getClass());
     }
 }
