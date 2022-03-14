7fea46c29ed39f9e06cd6f0b79f0055666b9243e
darren <2818663415@qq.com>
2022-03-14 18:05:17 +0800

    [protobuf] 初识protobuf

---
+++ demo-elasticsearch/demo-elasticsearch/src/test/java/com/protobuf/ProtoBufTest.java
@@ -0,0 +1,18 @@
+package com.protobuf;
+
+import com.jet.protobuf.PersonTestProtos;
+
+/**
+ * @author : darren
+ * @date : 2022/3/14
+ */
+public class ProtoBufTest {
+
+    public static void main(String[] args) {
+
+        //生成personTest对象
+        PersonTestProtos.PersonTest.Builder personBuilder = PersonTestProtos.PersonTest.newBuilder();
+        //PersonTest赋值
+
+    }
+}
