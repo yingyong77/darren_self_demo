a779fa9b7c54a2bd9f603ef90b46bf1a90d7377e
darren <2>
2022-03-21 09:33:02 +0800

    [netty] 分配handler处理的线程池

--- demo-elasticsearch/demo-elasticsearch/src/test/java/com/netty/netty/EventLoopServer.java
+++ demo-elasticsearch/demo-elasticsearch/src/test/java/com/netty/netty/EventLoopServer.java
@@ -5,6 +5,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.DefaultEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -20,13 +21,23 @@ import java.nio.charset.Charset;
 public class EventLoopServer {
 
     public static void main(String[] args) {
+        DefaultEventLoopGroup defaultEventLoopGroup = new DefaultEventLoopGroup();
         new ServerBootstrap()
+                //boss 和worker
+                //boss 只负责serverSocketChannel 上accept事件，worker只负责socketChannel上的读写
                 .group(new NioEventLoopGroup(2))
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        ch.pipeline().addLast("handler1", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf byteBuf = (ByteBuf) msg;
+                                log.debug(byteBuf.toString(Charset.defaultCharset()));
+                                ctx.fireChannelRead(msg); //将消息传递给下一个handler
+                            }
+                        }).addLast(defaultEventLoopGroup, "handler2", new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                 ByteBuf byteBuf = (ByteBuf) msg;
